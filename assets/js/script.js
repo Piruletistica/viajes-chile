569edcd3e7e9446709743d2778a2f10531b34b7f
piruletistica <francisca.moncada@gmail.com>
2022-02-04 18:35:01 -0300

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+//alert email button
+function email() {
+  alert("El correo fue enviado correctamente...");
+}
+//tooltip activate
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+ // smooth scroll
+ $(document).ready(function(){
+$("a").click(function(){
+ var go = this.hash
+
+  $("html, body").animate(
+    {
+      scrolltop: $(go).offset().top}, 800)
+  
+
+    })
+  })
+ 
+
+//Desaparecer y aparecer card-body
+$("#card1").click(function() {
+  $("#crb1").toggle("slow", function() {
+    // Animation complete.
+  });
+});
+
+$("#card2").click(function() {
+  $("#crb2").toggle("slow", function() {
+    // Animation complete.
+  });
+});
+
+$("#card3").click(function() {
+  $("#crb3").toggle("slow", function() {
+    // Animation complete.
+  });
+});
